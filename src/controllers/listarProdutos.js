8f65ac13db811283a040e67d06c814999dd514da
Deyweson <devdeyve@gmail.com>
2023-12-22 12:51:57 -0300

    :sparkles: feat: Rota de listagem dos produtos para o Usuário

---
+++ src/controllers/listarProdutos.js
@@ -0,0 +1,11 @@
+const pool = require('../../src/config/conexaoDB');
+
+const listarProdutos = async (request, response) => {
+    const query = 'select nome_produto, valor, descricao from produtos where em_estoque = true';
+
+    const produtos = await pool.query(query);
+
+    return response.status(200).json(produtos.rows);
+};
+
+module.exports = listarProdutos;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const listarProdutos = require('../controllers/listarProdutos');
 const routes = express();
+
+routes.get('/produtos', listarProdutos);
 
 module.exports = routes;
