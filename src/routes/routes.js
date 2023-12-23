9ea7614a3cd798b228f912ef8289b4b515382527
Deyweson <devdeyve@gmail.com>
2023-12-23 08:25:26 -0300

    :sparkles: feat: Rota de cadastro dos produtos

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const listarProdutos = require('../controllers/listarProdutos');
+const cadastrarProd = require('../controllers/cadastrarProd');
 const routes = express();
 
 routes.get('/produtos', listarProdutos);
+routes.post('/cadastrarProduto', cadastrarProd);
 
 module.exports = routes;
