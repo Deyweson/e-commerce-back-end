48bb3f37332e77b39588cc2475f6e35a717313d4
Deyweson <devdeyve@gmail.com>
2023-12-25 20:14:50 -0300

    :sparkles: feat: Rota de atualização de produto

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,16 +2,18 @@ const express = require('express');
 const listarProdutos = require('../controllers/public/listarProdutos');
 const cadastrarProd = require('../controllers/private/cadastrarProd');
 const login = require('../controllers/private/login');
-const autenticador = require('../../src/middleware/autenticador')
+const autenticador = require('../../src/middleware/autenticador');
+const editarProd = require('../controllers/private/editarProd');
 const routes = express();
 
 routes.get('/produtos', listarProdutos);
 
 
+
 routes.post('/login', login);
 routes.use(autenticador);
 routes.post('/cadastrarProduto', cadastrarProd);
-
+routes.post('/editarProduto', editarProd);
 
 
 module.exports = routes;
